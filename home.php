2006a28806a53c0739f0965dfb5c7c0cd9c90139
ntsyaadhyles <natasyadiahayu11@gmail.com>
2022-08-05 21:40:33 +0700

    natasya

---
+++ home.php
@@ -0,0 +1,12 @@
+<b>
+    <font color="#f5c772">DASHBOARD</font>
+</b>
+<br><br>
+<hr color="#f5c772" size="2px" width="100%">
+<br>
+<font color="#646464" size="+1">
+    <marquee>Selamat Datang Di Klinik Kami 
+        <b>SEMOGA LEKAS SEMBUH</b>
+    </marquee>
+</font>
+<br>
